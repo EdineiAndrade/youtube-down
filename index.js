30997fa11019ccde563ea64664d5042191e309a5
EdineiAndrade <edinei22.ba@gmail.com>
2022-03-20 09:35:14 -0300

    Alterando para index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express"); 
+const ytdl = require("ytdl-core");
+const app = express();
+
+app.get("/", function (req, res){
+    const {url} = req.query;
+
+    res.header("Content-Disposition", 'attachmentt; filename="Video.mp4"')
+    return ytdl(url).pipe(res);
+ 
+});
+
+app.listen(3000);
